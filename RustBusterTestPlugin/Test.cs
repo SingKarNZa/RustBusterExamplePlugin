1a11fe326c55399f12ff2b612e68b8456eefd098
DreTaX <dretax@gmail.hu>
2017-02-07 13:14:11 +0100

    1.0
    Signed-off-by: DreTaX <dretax@gmail.hu>

---
+++ RustBusterTestPlugin/Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RustBuster2016.API;
+using UnityEngine;
+
+namespace RustBusterTestPlugin
+{
+    public class Test : RustBusterPlugin
+    {
+        public TestGUI test;
+        private static GameObject Load;
+
+        public override string Name
+        {
+            get { return "TestPlugin"; }
+        }
+
+        public override string Author
+        {
+            get { return "DreTaX"; }
+        }
+
+        public override Version Version
+        {
+            get { return new Version("1.0"); }
+        }
+
+        public override void DeInitialize()
+        {
+            // DeInitialize gets called on disconnect, be sure to cleanup the user's client.
+            UnityEngine.Object.Destroy(Load);
+        }
+
+        public override void Initialize()
+        {
+            Load = new GameObject();
+            test = Load.AddComponent<TestGUI>();
+            UnityEngine.Object.DontDestroyOnLoad(Load);
+            UnityEngine.Debug.Log("Loaded!");
+        }
+    }
+}
---
+++ RustBusterTestPlugin/TestGUI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace RustBusterTestPlugin
+{
+    public class TestGUI : MonoBehaviour
+    {
+        
+        public void OnGUI()
+        {
+            // Make a background box
+            GUI.Box(new Rect(10, 10, 100, 90), "Test Menu");
+
+            // Make the first button. If it is pressed, Rust.Notice.Popup will be executed
+            if (GUI.Button(new Rect(20, 40, 80, 20), "Test"))
+            {
+                Rust.Notice.Popup("", "Test");
+            }
+        }
+    }
+}
